bb85772bda50fc0e2abe7525932c0291680d6c9d
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-04-29 19:18:01 -0400

    Initial Commit

---
+++ HelloWorld/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HelloWorld
+//
+//  Created by Ben Godfrey on 4/29/20.
+//  Copyright © 2020 Ben Godfrey. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
